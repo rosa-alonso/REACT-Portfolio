da4e43cb97099c443690b49c111632c7257b6408
Rosa Alonso <alonso_2018@yahoo.com>
2023-07-11 21:57:04 -0500

    worked on my src files

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,1 +1,24 @@
-export default function Contact() {}
+import React from "react";
+
+export default function Contact() {
+  <section id="contact" className="relative">
+    <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+          <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+              EMAIL
+            </h2>
+            <param className="text-indigo-400 leading-relaxed">
+              alonso_2018@yahoo.com
+            </param>
+            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+              PHONE
+            </h2>
+            <p className="leading-relaxed">512-525-6627</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>;
+}
---
+++ my-portfolio/src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "Dinner Genie",
+    subtitle: "First Project",
+    description:
+      "This was a front-end project done with a few of my classmates. The website allows users to find dinner recipe based on three main ingredients of their choosing",
+    image: "./project-1.gif", //add pics
+    link: "https://landywandy.github.io/Dinner-Genie/",
+  },
+  {
+    title: "Task Hub",
+    subtitle: "Task Management App",
+    description:
+      "This app was created as a task manager with a social media feel to it, users compete with their friends to complete their daily tasks. This app was created with my group members as a project.",
+    image: "./project-2.gif",
+    link: "https://taskhub.herokuapp.com/login",
+  },
+  {
+    title: "E-Commerce Site",
+    subtitle: "ORM application",
+    description:
+      "This is an ORM application created to demonstrate connecting multiple models and creating CRUD routes.",
+    image: "./project-3.gif",
+    link: "https://github.com/rosa-alonso/e-commerce-site",
+  },
+];
