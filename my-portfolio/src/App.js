3c6bb6a571ad1092706c0b6f4405906116242e7f
Rosa Alonso <alonso_2018@yahoo.com>
2023-07-11 20:16:27 -0500

    created REACT app and started working in src folder

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
